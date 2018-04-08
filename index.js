0bdea254d020cf043601e80e739c4a003af7ed9e
MatWidOZS <36623352+MatWidOZS@users.noreply.github.com>
2018-04-08 22:04:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var colors = require('colors');
 
 fs.readdir('./', 'utf-8', function(err, files) {
+	if (err) throw err;
 	console.log(files);
 	fs.writeFile('./files.txt', files, function(err) {
 		if (err) throw err;
